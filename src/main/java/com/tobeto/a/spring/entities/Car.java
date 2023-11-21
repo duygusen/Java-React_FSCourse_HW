13e02bf719ef43249c27e18d54e360f9f3abda0f
duygusen <116661390+duygusen@users.noreply.github.com>
2023-11-22 01:21:38 +0300

    Workshop/171123

---
+++ src/main/java/com/tobeto/a/spring/entities/Car.java
@@ -0,0 +1,46 @@
+package com.tobeto.a.spring.entities;
+
+import jakarta.persistence.*;
+
+@Table(name="cars")
+@Entity
+public class Car
+{
+    @Id
+    @Column(name="id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private  int id;
+
+    @Column(name="model_year")
+    private int modelYear;
+
+    @Column(name="model_name")
+    private String modelName;
+
+    @Column(name="daily_price")
+    private double dailyPrice;
+
+    @Column(name="color")
+    private String color;
+
+    @Column(name="status")
+    private String status;
+
+
+    @ManyToOne
+    @JoinColumn(name = "brand_id")
+    private Brand brand;
+
+    @ManyToOne
+    @JoinColumn(name="employee_id")
+    private Employee employee;
+
+    @ManyToOne
+    @JoinColumn(name="payment_id")
+    private Payment payment;
+
+    @ManyToOne
+    @JoinColumn(name="customer_id")
+    private Customer customer;
+
+}
---
+++ src/main/java/com/tobeto/a/spring/entities/Employee.java
@@ -0,0 +1,36 @@
+package com.tobeto.a.spring.entities;
+
+import jakarta.persistence.*;
+
+import java.util.List;
+
+@Table(name="employees")
+@Entity
+public class Employee
+{
+    @Id
+    @Column(name="id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private  int id;
+
+    @Column(name="name")
+    private String name;
+
+    @Column(name="surname")
+    private String surname;
+
+    @Column(name="age")
+    private int age;
+
+    @Column(name="adress")
+    private String adress;
+
+    @Column(name="phone")
+    private int phone;
+
+    @OneToMany(mappedBy = "employee")
+    private List<Car> cars;
+    @OneToMany(mappedBy = "employee")
+    private List<Payment> payment;
+
+}
